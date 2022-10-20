22aba8fdb394925060a15f0f078439cdf8559905
ChillyVanilly <vasilii.gurev@hs-furtwangen.de>
2022-10-20 14:38:12 +0200

    started updating

--- Mischa/Script/Build/Script.js
+++ Mischa/Script/Build/Script.js
@@ -1,1 +1,67 @@
-console.warn("%cFor scripting, start your favorite IDE (e.g. VSCode) with typescript installed, load and compile this project", "color: red; font-weight: bold;");
+"use strict";
+var Script;
+(function (Script) {
+    var ƒ = FudgeCore;
+    ƒ.Project.registerScriptNamespace(Script); // Register the namespace to FUDGE for serialization
+    let CustomComponentScript = /** @class */ (() => {
+        class CustomComponentScript extends ƒ.ComponentScript {
+            constructor() {
+                super();
+                // Properties may be mutated by users in the editor via the automatically created user interface
+                this.message = "CustomComponentScript added to ";
+                // Activate the functions of this component as response to events
+                this.hndEvent = (_event) => {
+                    switch (_event.type) {
+                        case "componentAdd" /* COMPONENT_ADD */:
+                            ƒ.Debug.log(this.message, this.node);
+                            break;
+                        case "componentRemove" /* COMPONENT_REMOVE */:
+                            this.removeEventListener("componentAdd" /* COMPONENT_ADD */, this.hndEvent);
+                            this.removeEventListener("componentRemove" /* COMPONENT_REMOVE */, this.hndEvent);
+                            break;
+                        case "nodeDeserialized" /* NODE_DESERIALIZED */:
+                            // if deserialized the node is now fully reconstructed and access to all its components and children is possible
+                            break;
+                    }
+                };
+                // Don't start when running in editor
+                if (ƒ.Project.mode == ƒ.MODE.EDITOR)
+                    return;
+                // Listen to this component being added to or removed from a node
+                this.addEventListener("componentAdd" /* COMPONENT_ADD */, this.hndEvent);
+                this.addEventListener("componentRemove" /* COMPONENT_REMOVE */, this.hndEvent);
+                this.addEventListener("nodeDeserialized" /* NODE_DESERIALIZED */, this.hndEvent);
+            }
+        }
+        // Register the script as component for use in the editor via drag&drop
+        CustomComponentScript.iSubclass = ƒ.Component.registerSubclass(CustomComponentScript);
+        return CustomComponentScript;
+    })();
+    Script.CustomComponentScript = CustomComponentScript;
+})(Script || (Script = {}));
+var Script;
+(function (Script) {
+    var ƒ = FudgeCore;
+    var ƒAid = FudgeAid;
+    ƒ.Debug.info("Main Program Template running!");
+    //initialize Viewport
+    let viewport;
+    document.addEventListener("interactiveViewportStarted", start);
+    function start(_event) {
+        viewport = _event.detail;
+        ƒ.Loop.addEventListener("loopFrame" /* LOOP_FRAME */, update);
+        // ƒ.Loop.start();  // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
+    }
+    let walkAnimation;
+    function initializeAnimations(coat) {
+        walkAnimation = new ƒAid.SpriteSheetAnimation("Walk", coat);
+        walkAnimation.generateByGrid(ƒ.Rectangle.GET(0, 0, 16, 24), 2, 64, ƒ.ORIGIN2D.BOTTOMCENTER, ƒ.Vector2.X(16));
+        //load Sprite
+        function update(_event) {
+            // ƒ.Physics.simulate();  // if physics is included and used
+            viewport.draw();
+            ƒ.AudioManager.default.update();
+        }
+    }
+})(Script || (Script = {}));
+//# sourceMappingURL=Script.js.map
--- Mischa/Script/Source/Main.ts
+++ Mischa/Script/Source/Main.ts
@@ -1,7 +1,9 @@
 namespace Script {
   import ƒ = FudgeCore;
+  import ƒAid = FudgeAid;
   ƒ.Debug.info("Main Program Template running!");
 
+  //initialize Viewport
   let viewport: ƒ.Viewport;
   document.addEventListener("interactiveViewportStarted", <EventListener>start);
 
@@ -12,9 +14,21 @@ namespace Script {
     // ƒ.Loop.start();  // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
   }
 
+  let walkAnimation: FudgeAid.SpriteSheetAnimation;
+  function initializeAnimations(coat: ƒ.CoatTextured) {
+    walkAnimation = new ƒAid.SpriteSheetAnimation("Walk", coat);
+    walkAnimation.generateByGrid(ƒ.Rectangle.GET(0, 0, 16, 24), 2, 64, ƒ.ORIGIN2D.BOTTOMCENTER, ƒ.Vector2.X(16));
+
+//load Sprite
+
+
+
+
+
   function update(_event: Event): void {
     // ƒ.Physics.simulate();  // if physics is included and used
     viewport.draw();
     ƒ.AudioManager.default.update();
   }
+  
 }
